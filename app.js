4c487021dc6c9d88589aea087a4bc45ab341a780
rupyb <rupybrisman@yahoo.com>
2019-02-06 19:05:17 -0500

    new

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('../../blogapp/dist/blogapp'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 // app.use('/', indexRouter);
