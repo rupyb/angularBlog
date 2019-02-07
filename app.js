6d9e02ccab8a9aea349cd50b1a4a554b6c699fd7
rupyb <rupybrisman@yahoo.com>
2019-02-06 21:20:33 -0500

    new

--- app.js
+++ app.js
@@ -63,12 +63,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'dist/blogapp')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.get('/', (req, res, next) => {
+app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'dist/blogapp'));
 });
 app.get('/stuff', (req, res, next) => {
